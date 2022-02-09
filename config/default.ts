0b43d69e26f93801fed1729830c70bd0a0e09c1b
Emmanuel Yirenkyi <yirenemma@gmail.com>
2022-02-09 13:58:34 +0000

    Added a config file

---
+++ config/default.ts
@@ -0,0 +1,9 @@
+export default {
+  port: 3350,
+  etherApiKey: 'IUVDD28X7E9ZUCZ3Q23NVT6391H1NST35X',
+  etherMainnet: 'https://api.etherscan.io/api',
+  etherGoerli: 'https://api-goerli.etherscan.io/api',
+  etherKovan: 'https://api-kovan.etherscan.io/api',
+  etherRinkeby: 'https://api-rinkeby.etherscan.io/api',
+  etherRopsten: 'https://api-ropsten.etherscan.io/api',
+};
