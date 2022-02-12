bfb9d51f534028d92f0aca88ce7cb4b0fce6df80
Emmanuel Yirenkyi <yirenemma@gmail.com>
2022-02-12 15:58:52 +0000

    Heroku

--- src/docs/servers.ts
+++ src/docs/servers.ts
@@ -4,6 +4,10 @@ const port = process.env.PORT;
 const apiServers = {
   servers: [
     {
+      url: 'https://eth-reader.herokuapp.com/api',
+      description: 'Staging',
+    },
+    {
       url: `http://localhost:${port}/api`,
       description: 'Local Server',
     },
